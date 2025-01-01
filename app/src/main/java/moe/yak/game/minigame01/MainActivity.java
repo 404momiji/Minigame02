2f60c2c3872e56502640e61124f4b63cb6f4592b
焼きシジミ <yakisizimi@gmail.com>
2025-01-01 15:16:10 +0900

    エクスペリエンス修正
    残り秒数表示等追加
    そのほか

--- app/src/main/java/moe/yak/game/minigame01/MainActivity.java
+++ app/src/main/java/moe/yak/game/minigame01/MainActivity.java
@@ -1,13 +1,8 @@
 package moe.yak.game.minigame01;
 
-/*
-//Next do 残り時間を表示する
-//
-*/
 
 
-
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.hardware.Sensor;
@@ -17,10 +12,10 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.view.Display;
 import android.view.View;
-import android.view.WindowManager;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -28,17 +23,18 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends Activity implements SensorEventListener {
+public class MainActivity extends SensorGameActivity implements SensorEventListener {
 
     float rand;
-    float nsx;
+
     float nsy;
-    float nsz;
+    int timeSec = 25;
     float outLeftPad;
     float outRightPad;
     float center;
     float playerPosition;
     int moveCount = 0;
+    
 
     boolean animated = true;
     public int animationCount = 0;
@@ -47,34 +43,23 @@ public class MainActivity extends Activity implements SensorEventListener {
     boolean outed = true;
     int nowCount = 0;
     int playerPositionCount = 0;
-    public int mTime;
     int resetPosition = 0;
 
-    final int nmcount = 50;
-
     float W = 0;
-    int taped = 0;
     Handler mHandler;//るいぱんこ
     Handler aHandler;//アニメーション用ハンドラ
-
-
-    boolean Handed = true;
-
     Timer mTimer;
     Timer aTimer;//アニメーションタイマー
-
     private SensorManager manager;
-
     public int width = 0;
-    TextView txtv;
     //debugviewのインスタンスをしゅとくすりゅぅうぅぅぅぅうぅ
-    TextView dbgv;
+    RelativeLayout lo;
+    TextView dbg_TextView;
     //NOW statusとるお
     TextView clearText;
-    TextView nkr;
+    TextView Nokori_TextView;
     ImageView playerImage;
     public float playerNowPositionX = 0;
-    public float nTX = 0;
 
 
 
@@ -87,44 +72,50 @@ public class MainActivity extends Activity implements SensorEventListener {
         setContentView(R.layout.activity_main);
 
         animated = true;
-        //ハンドラー取得すりゅ♥
+
         mHandler = new Handler();
-        //バグの原因っぽい（下が抜けてた）
         aHandler = new Handler();
 
         //横画面固定
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        //Windowマネージャをよびだしゅぅぅぅぅぅぅぅぅ
-        WindowManager wm = getWindowManager();
-        //Dispのインスタンスを取得すりゅううううう
-        Display disp = wm.getDefaultDisplay();
-        width = disp.getWidth();
+        lo = findViewById(R.id.base);
+        ViewTreeObserver observer = lo.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(
+                () -> {
+                    initCalc(lo.getWidth());
+                    Log.d("WIDTH",lo.getWidth()+"");
+                });
+
         //センサーマネジャーからインスタンスをしゅとくすりゅぅうぅぅぅぅうぅ(はーと)
         manager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        dbgv = (TextView) findViewById(R.id.textView5);
-        txtv = (TextView) findViewById(R.id.myView2);
-        nkr = (TextView) findViewById(R.id.NOKORI);
-        clearText = (TextView) findViewById(R.id.nowst);
-        playerImage = (ImageView) findViewById(R.id.imageView4);
+        dbg_TextView =  findViewById(R.id.textView5);
+        if(ApplicationController.DBG_MODE){
+            dbg_TextView.setVisibility(View.VISIBLE);
+        }
+        Nokori_TextView = findViewById(R.id.NOKORI);
+        clearText = findViewById(R.id.nowst);
+        playerImage = findViewById(R.id.imageView4);
 
         playerNowPositionX = playerImage.getTranslationX();
-        //TX = txtv.getTranslationX();
-        center = width / 2;
-        playerPosition = center - 35;
+
 
         //プレイヤーを指定位置に設定する
-        //txtv.setTranslationX(plcenter);
         playerImage.setTranslationX(playerPosition);
 
-        //TXを初期化しなおす
-        //TX = txtv.getTranslationX();
+
         playerNowPositionX = playerImage.getTranslationX();
 
-        outLeftPad = (width - stage) / 2;
-        outRightPad = outLeftPad + stage;
+
 
     }
 
+    private void initCalc(int _width){
+        width = _width;
+        center = (float) width / 2;
+        playerPosition = center - 70;
+        outLeftPad = (width - stage) / 2-110;
+        outRightPad = outLeftPad + stage+150;
+    }
     public void start(View v) {
         moveCount = 0;
 
@@ -132,14 +123,11 @@ public class MainActivity extends Activity implements SensorEventListener {
         nowCount = 0;
         //プレイヤーを指定位置に設定する
         playerImage.setTranslationX(playerPosition);
-        //txtv.setTranslationX(plcenter);
 
         //playerNowPositionXを初期化しなおす
-        //playerNowPositionX = txtv.getTranslationX();
         playerNowPositionX = playerImage.getTranslationX();
 
         clearText.setVisibility(View.INVISIBLE);
-        //txtv.setTranslationX(plcenter);
         playerImage.setTranslationX(playerPosition);
         outed = false;
         mTimer = new Timer(false);
@@ -147,18 +135,19 @@ public class MainActivity extends Activity implements SensorEventListener {
             @Override
             public void run() {
                 mHandler.post(new Runnable() {
+                    @SuppressLint("SetTextI18n")
                     @Override
                     public void run() {
                         if (outed) {
                             cancel();
                         }
-                        if (animated == true) {
+                        if (animated) {
                             if ((nowCount % 3) == 0 && !(nowCount == 0)) {
 
                                 if (moveCount == 0) {
                                     Random bol = new Random();
 
-                                    resetPosition = bol.nextInt(1);
+                                    resetPosition = bol.nextInt(3);
                                     if (resetPosition == 0) {
                                         playerNowPositionX = outLeftPad - 5;
                                         setPlayerPosition(playerNowPositionX);
@@ -177,7 +166,7 @@ public class MainActivity extends Activity implements SensorEventListener {
                             if (rand == 0) {
                                 rand = random.nextInt(20);
                             }
-                            nkr.setText(rand + ":RAND");
+                            Nokori_TextView.setText("残り:"+(timeSec-nowCount/2) + "秒");
                             if (10 > rand) {
                                 rand = rand * -1;
                             } else if (rand == 10) {
@@ -185,12 +174,12 @@ public class MainActivity extends Activity implements SensorEventListener {
                             } else if (10 < rand) {
                                 rand = rand - 10;
                             }
-                            animation(rand);
+                            animate(rand);
                         }
                         //TX = TX + rand;
                         //↓移動処理
                         //ST(TX);
-                        if (nowCount >= 50) {
+                        if (nowCount >= timeSec*2) {
                             clear();
                             cancel();
                         }
@@ -212,66 +201,56 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     }
 
-    public float setPlayerPosition(float tx) {
-        if (outed == false) {
+    public void setPlayerPosition(float tx) {
+        if (!outed) {
             playerImage.setTranslationX(tx);
-            //txtv.setTranslationX(tx);
         }
-        return 0;
     }
 
-    public float animation(float wei) {
+    public void animate(float wei) {
         playerPositionCount = 1;
 
         animationCount = 1;
         final int actm = 20;//20周期で終了（）
 
         W = wei / 20;
-        float anmct;
         aTimer = new Timer(false);
         aTimer.schedule(new TimerTask() {
             @Override
-            public void run() {//ここ等辺でおちる
-                aHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
+            public void run() {
+                aHandler.post(() -> {
 
-                        animationCount++;
-                        playerNowPositionX = (W * playerPositionCount) + playerNowPositionX;
-                        setPlayerPosition(playerNowPositionX);
-                        if (animationCount >= actm) {
-                            animated = true;
-                            cancel();
-                        }
-                        playerPositionCount++;
+                    animationCount++;
+                    playerNowPositionX = (W * playerPositionCount) + playerNowPositionX;
+                    setPlayerPosition(playerNowPositionX);
+                    if (animationCount >= actm) {
+                        animated = true;
+                        cancel();
                     }
+                    playerPositionCount++;
                 });
 
             }
         }, 0, 50);
 
 
-        return 0;
     }
 
 
     public void aclk(float weg) {
-        if (outed == false) {
+        if (!outed) {
             if (playerNowPositionX < width - 20) {
                 playerNowPositionX = playerNowPositionX + weg;
                 playerImage.setTranslationX(playerNowPositionX);
-                //txtv.setTranslationX(TX);
             }
         }
     }
 
     public void bclk(float weg) {
-        //nTX
-        if (outed == false) {
+        if (!outed) {
             if (playerNowPositionX > 0) {
                 playerNowPositionX = playerNowPositionX + weg;
                 playerImage.setTranslationX(playerNowPositionX);
-                //txtv.setTranslationX(TX);
             }
         }
     }
@@ -289,26 +268,23 @@ public class MainActivity extends Activity implements SensorEventListener {
         //リスナーの登録
         List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ACCELEROMETER);
 
-        if (sensors.size() > 0) {
+        if (!sensors.isEmpty()) {
             Sensor s = sensors.get(0);
-
             manager.registerListener(this, s, SensorManager.SENSOR_DELAY_GAME);
-
         }
     }
 
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        //TODO AUTO-generated method Stub
-    }
 
+
+    @SuppressLint("SetTextI18n")
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            nsx = event.values[SensorManager.DATA_X];
+            //nsx = event.values[SensorManager.DATA_X];
             nsy = event.values[SensorManager.DATA_Y];
-            //Log.i("AAA",String.valueOf(nsy));
+            //nsz = event.values[SensorManager.DATA_Y];
+
 
             if (nsy >= 4.0f) {
                 moveCount++;
@@ -317,11 +293,12 @@ public class MainActivity extends Activity implements SensorEventListener {
                 moveCount++;
                 bclk(nsy);
             }
-            dbgv.setText("playerNowPositionX:" + playerNowPositionX);
+            if(ApplicationController.DBG_MODE) {
+                dbg_TextView.setText("playerNowPositionX:" + playerNowPositionX);
+            }
 
             if (!outed) {
-                if (playerNowPositionX >= outLeftPad - 70 && playerNowPositionX <= outRightPad - 60) {
-                } else {
+                if (!(playerNowPositionX >= outLeftPad - 70) || !(playerNowPositionX <= outRightPad - 60)) {
                     Log.d("AAA", "アウトを検知");
                     clearText.setTextColor(Color.RED);
                     clearText.setText("アウトです♥");
@@ -330,9 +307,10 @@ public class MainActivity extends Activity implements SensorEventListener {
                     outed = true;
                 }
             }
-            nsz = event.values[SensorManager.DATA_Z];
+
 
 
         }
     }
+
 }
---
+++ app/src/main/java/moe/yak/game/minigame01/SensorGameActivity.java
@@ -0,0 +1,18 @@
+package moe.yak.game.minigame01;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class SensorGameActivity extends Activity implements SensorEventListener {
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
